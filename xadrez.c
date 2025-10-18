1acff8fb3fc604082fe09a13053d688629394fd9
Pedro Lucas <pedroo.llucas007@gmail.com>
2025-10-18 10:13:39 -0300

    Atualização do Nível Novato - movimentos Torre, Bispo e Rainha

--- xadrez.c
+++ xadrez.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
-int main() {
-    printf("Ambiente pronto para o Xadrez! \n");
+
+void separador(const char *titulo) {
+    printf("\n=== %s ===\n", titulo);
+}
+
+int main(void) {
+    separador("Torre (for) Direita x5");
+    for (int i = 0; i < 5; i++) {
+        puts("Direita");
+    }
+
+    separador ("Bispo (while) - Cima Direita x5");
+    int passos = 0;
+    while (passos < 5){
+    puts("Cima Direita");
+    passos = passos + 1;
+    }
+
+    separador("Rainha (do-while) - Esquerda x8");
+    int n = 0;
+    do {
+        puts("Esquerda");
+        n++;
+    } while (n < 8);
+    
     return 0;
+
 }
