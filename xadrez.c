03104514291f949820484a85d58b78d53812af61
Pedro Lucas <pedroo.llucas007@gmail.com>
2025-10-30 19:56:39 -0300

    Nível Aventureiro - funções e modularização

--- xadrez.c
+++ xadrez.c
@@ -1,29 +1,44 @@
 #include <stdio.h>
 
 void separador(const char *titulo) {
-    printf("\n=== %s ===\n", titulo);
+     printf("\n=== %s ===\n", titulo);
 }
 
-int main(void) {
-    separador("Torre (for) Direita x5");
-    for (int i = 0; i < 5; i++) {
+//Torre
+void moverTorre(void) {
+    separador("Torre (for) - Direita x5");
+     for (int i = 0; i < 5; i++) {
         puts("Direita");
-    }
+          }
+}
 
-    separador ("Bispo (while) - Cima Direita x5");
+//Bispo
+void moverBispo(void) {
+    separador("Bispo (while) - Cima Direita x5");
     int passos = 0;
-    while (passos < 5){
-    puts("Cima Direita");
-    passos = passos + 1;
+    while (passos < 5) {
+        puts("Cima Direita");
+        passos++;
     }
+}
 
-    separador("Rainha (do-while) - Esquerda x8");
-    int n = 0;
-    do {
+//Rainha
+void moverRainha(void) {
+     separador("Rainha (do-while) - Esquerda x8");
+      int n = 0;
+       do {
         puts("Esquerda");
         n++;
     } while (n < 8);
-    
-    return 0;
+}
 
+int main(void) {
+    separador("Iniciando Nível Aventureiro - Movimentos das Peças");
+
+    moverTorre();
+    moverBispo();
+    moverRainha();
+
+    separador("Fim do Nível Aventureiro!");
+    return 0;
 }
